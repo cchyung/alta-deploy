324001e251cdafc1129ccc7cbc9e0e40662a39ca
Conner Chyung <chyungspice@gmail.com>
2016-07-30 10:50:17 -0600

    updated css for testimonials

---
+++ static/website/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    navbarController();
+    $("#contact-btn").click(function () {
+        window.location.href = 'mailto:melissawoodvine@hotmail.com?Subject=Alta Tutoring Inquiry';
+        console.log("Got clicked!")
+    });
+});
+
+// navbar controller
+
+var OFFSET = 200;
+function navbarController() {
+    var triggerPosition = $('.nav-trigger').position().top + OFFSET;
+    $(window).on("scroll", function () {
+            var scrollPosition = scrollY || pageYOffset;
+            if (scrollPosition > triggerPosition) {
+                $('header').addClass('nav-fixed');
+                var navHeight = $(".nav-container").css('height');
+                $('.nav-ghost').height(navHeight);
+                $('.nav-ghost').show();
+            } else {
+                $('header').removeClass('nav-fixed');
+            }
+
+
+        }
+    )
+    ;
+}
